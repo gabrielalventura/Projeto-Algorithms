d402806edffcd0a670e4fa37b2b72fcc0d0159c4
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-02 13:26:58 -0300

    Fix: Corrige o tipo de retorno esperado em permanence_period

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,15 @@
 def study_schedule(permanence_period, target_time):
     more_students = 0
 
-    if not permanence_period and permanence_period != int or not target_time:
+    if (
+        not permanence_period
+        or type(permanence_period) != list
+        or not target_time
+    ):
         return None
 
     for time in permanence_period:
+
         if time[0] <= target_time <= time[1]:
             more_students += 1
 
