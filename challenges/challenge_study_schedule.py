f3937b849375f387a66658d1da9fe0bae829ac87
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-02 12:25:09 -0300

    Feat: Implementa função study_schedule mas ainda não passa em todas as verificações

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    more_students = 0
+
+    if not permanence_period and permanence_period != int or not target_time:
+        return None
+
+    for time in permanence_period:
+        if time[0] <= target_time <= time[1]:
+            more_students += 1
+
+    return more_students
