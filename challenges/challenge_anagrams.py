ba639509b650edda4d567bacae0ada54e9212b38
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-03 06:34:16 -0300

    Refact: Passar a usar .append em merge para correção de erro 'IndexError' indicado nos testes, ver referencia de solução no código

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,23 +3,28 @@
 # merge e merge_sort obtidos através da aula 2.3
 # disponivel em :
 # https://github.com/tryber/sd-025-b-live-lectures/blob/lecture/cs/2.3/sort/merge_sort.py
+# referencia IndexError:
+# https://bobbyhadz.com/blog/python-indexerror-list-assignment-index-out-of-range#:~:text=The%20Python%20%22IndexError%3A%20list%20assignment,of%20the%20list%2C%20e.g.%20my_list.
 
-def merge(left, right, merged):
+def merge(left, right):
+    merged = []
     left_p, right_p = 0, 0
 
     while left_p < len(left) and right_p < len(right):
-        if left[left_p] <= right[right_p]:
-            merged[left_p + right_p] = left[left_p]
+        if left[left_p] < right[right_p]:
+            merged.append(left[left_p])
             left_p += 1
         else:
-            merged[left_p + right_p] = right[right_p]
+            merged.append(right[right_p])
             right_p += 1
 
-    for left_p in range(left_p, len(left)):
-        merged[left_p + right_p] = left[left_p]
+    while left_p < len(left):
+        merged.append(left[left_p])
+        left_p += 1
 
-    for right_p in range(right_p, len(right)):
-        merged[right_p + left_p] = right[right_p]
+    while right_p < len(right):
+        merged.append(right[right_p])
+        right_p += 1
 
     return merged
 
@@ -30,7 +35,7 @@ def merge_sort(info):
 
     mid = len(info) // 2
     left, right = merge_sort(info[:mid]), merge_sort(info[mid:])
-    return merge(left, right, info.copy())
+    return merge(left, right)
 
 
 def is_anagram(first_string, second_string):
@@ -40,10 +45,10 @@ def is_anagram(first_string, second_string):
     ):
         return first_string, second_string, False
 
-    f_string = list(first_string.lower())
-    s_string = list(second_string.lower())
+    f_string = first_string.lower()
+    s_string = second_string.lower()
 
-    mix_one = ''.join(merge_sort(f_string))
-    mix_two = ''.join(merge_sort(s_string))
+    mix_one = "".join(merge_sort(list(f_string)))
+    mix_two = "".join(merge_sort(list(s_string)))
 
-    return mix_one == mix_two
+    return (mix_one, mix_two, mix_one == mix_two)
